f5b4297ce97e12b6553b7f527f1ea3a33895d98f
casarin <nsgbanks@ciencias.unam.mx>
2023-05-07 13:48:29 -0600

    ApiException created to handle problems related to actions

---
+++ michelin/src/main/java/com/michelin/exception/ApiException.java
@@ -0,0 +1,18 @@
+package com.michelin.exception;
+
+public class ApiException extends RuntimeException {
+    private HttpStatus status;
+
+    public ApiException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    } 
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;        
+    }   
+}
