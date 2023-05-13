846aaf868f7527b524e29901b05846711ce087e7
casarin <nsgbanks@ciencias.unam.mx>
2023-05-13 17:43:24 -0600

    RepoProduct added

---
+++ michelin/src/main/java/com/michelin/api/repository/RepoProduct.java
@@ -0,0 +1,14 @@
+package com.michelin.api.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.michelin.api.entity.Product;
+
+public interface RepoProduct extends JpaRepository<Product,Integer>{
+    
+}
