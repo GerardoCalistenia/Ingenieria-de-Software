84c5e8f16695680dd4ada9196d9ba29f11335fc5
Jorge Escorza <jorgezulan@gmail.com>
2023-05-21 21:26:28 -0600

    html conectado al backend

---
+++ michelin/src/main/java/com/michelin/api/controller/CtrlLogin.java
@@ -0,0 +1,15 @@
+package com.michelin.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/michelin/login")
+public class CtrlLogin {
+
+    @GetMapping
+    public String loginPage(){
+        return "login";
+    }
+}
---
+++ michelin/src/main/resources/static/js/loginscript-0.1.js
@@ -0,0 +1,100 @@
+
+
+document.getElementById("btn_registrarse").addEventListener("click", register);
+document.getElementById("btn_iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("registrar").addEventListener("click", registro);
+document.getElementById("entrar").addEventListener("click", entrar);
+window.addEventListener("resize", anchoPagina);
+
+// Variables
+var contenedor_login_register = document.querySelector(".contenedor_login-register");
+var formulario_login = document.querySelector(".formulario_login");
+var formulario_register = document.querySelector(".formulario_register");
+var caja_trasera_login = document.querySelector(".caja_trasera_login");
+var caja_trasera_register = document.querySelector(".caja_trasera_register");
+
+function anchoPagina(){
+	if(window.innerWidth > 850){
+		caja_trasera_login.style.display = "block";
+		caja_trasera_register.style.display = "block";
+	}else{
+		caja_trasera_register.style.display = "block";
+		caja_trasera_register.style.opacity = "1";
+		caja_trasera_login.style.display = "none";
+		formulario_login.style.display = "block";
+		formulario_register.style.display = "none";
+		contenedor_login_register.style.left = "8px";
+	}
+}
+
+anchoPagina();
+
+function iniciarSesion() {
+
+	if (innerWidth > 850) {
+		formulario_register.style.display = "none";
+		contenedor_login_register.style.left = "0px";
+		formulario_login.style.display = "block";
+		caja_trasera_register.style.opacity = "1";
+		caja_trasera_login.style.opacity = "0";
+	} else {
+		formulario_register.style.display = "none";
+		contenedor_login_register.style.left = "0px";
+		formulario_login.style.display = "block";
+		caja_trasera_register.style.display = "block";
+		caja_trasera_login.style.display = "none";
+		contenedor_login_register.style.top = "2"; // Ajusta el valor según el ancho del contenedor
+	}
+}
+
+function register() {
+
+	if (innerWidth > 850) {
+		formulario_register.style.display = "block";
+		contenedor_login_register.style.left = "410px";
+		formulario_login.style.display = "none";
+		caja_trasera_register.style.opacity = "0";
+		caja_trasera_login.style.opacity = "1";
+	} else {
+		formulario_register.style.display = "block";
+		contenedor_login_register.style.left = "0px";
+		formulario_login.style.display = "none";
+		caja_trasera_register.style.display = "none";
+		caja_trasera_login.style.display = "block";
+		caja_trasera_login.style.opacity = "1";
+	}
+}
+
+function registro() {
+	event.preventDefault();
+	console.log("Hola");
+
+	const nombre = document.getElementById("nombreRegistro").value;
+    const email = document.getElementById("emailRegistro").value;
+
+	const usuario = {
+        nombre: nombre,
+        email: email
+    };
+
+	fetch('http://localhost:8081/michelin/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Hacer algo con los datos de respuesta
+    })
+    .catch(error => {
+        console.error('Error:', error); // Manejar cualquier error de la llamada
+    });
+
+}
+
+function entrar() {
+	event.preventDefault();
+	console.log("Hola");
+}
