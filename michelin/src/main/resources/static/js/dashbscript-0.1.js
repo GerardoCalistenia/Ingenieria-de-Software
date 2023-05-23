a83b5fdf56eaafcbeadafd3f59ef3d03cec3126d
SergioIARamirezMunoz <74012364+SergioIARamirezMunoz@users.noreply.github.com>
2023-05-23 12:52:17 -0600

    Quick script change.

--- michelin/src/main/resources/static/js/dashbscript-0.1.js
+++ michelin/src/main/resources/static/js/dashbscript-0.1.js
@@ -2,7 +2,7 @@ document.getElementsByClassName("btn btn-outline-secondary shadow-sm d-sm d-bloc
 document.getElementsByClassName("btn btn-outline-secondary shadow-sm d-sm d-block").addEventListener("click", actualiza);
 
 var cerrar_sesion = document.querySelector(".btn btn-outline-secondary shadow-sm d-sm d-block me-5");
-var cambiar_contra = document.querySelector("btn btn-outline-secondary shadow-sm d-sm d-block");
+var cambiar_contra = document.querySelector(".btn btn-outline-secondary shadow-sm d-sm d-block");
 
 function cierre() {
     const response = fetch('http://localhost:8081/michelin/login', {
