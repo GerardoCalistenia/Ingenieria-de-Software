f4162ca90d2b1fe1d0f880ec001e01b4df9c121d
SergioIARamirezMunoz <74012364+SergioIARamirezMunoz@users.noreply.github.com>
2023-06-05 18:27:37 -0600

    DaB script update.

--- michelin/src/main/resources/static/js/dashbscript-0.1.js
+++ michelin/src/main/resources/static/js/dashbscript-0.1.js
@@ -3,6 +3,8 @@ window.addEventListener("load", checkSession);
 var cookieValue = "";
 
 document.getElementById("boton_cerrar_sesion").addEventListener("click", logout);
+document.getElementById("boton_guardar_contrasena").addEventListener("click", updatePass)
+document.getElementById("boton_guardar_contrasena").addEventListener("keyup", enableDisable)
 
 function checkSession() {
   var cookieName = "nombreCookie";
@@ -57,3 +59,43 @@ function logout() {
 
 }
 
+function enableDisable() {
+    var btnGuardarContrasena = document.getElementById("boton_guardar_contrasena");
+    const nueva_contrasena = document.getElementById("nueva_contrasena").value();
+    const confirmar_contrasena = document.getElementById("confirmar_contrasena").value();
+    
+    if (nueva_contrasena != confirmar_contrasena || nueva_contrasena == "") {
+        btnGuardarContrasena.disabled = true;
+    } else {
+        btnGuardarContrasena.disabled = false;
+    }
+}
+
+function updatePass() {
+    event.preventDefault();
+    const nueva_contrasena = document.getElementById("nueva_contrasena").value();
+    const confirmar_contrasena = document.getElementById("confirmar_contrasena").value();
+
+    const update = {
+        nueva_contrasena: nueva_contrasena,
+        confirmar_contrasena: confirmar_contrasena
+    };
+
+    fetch('http://localhost:8081/michelin/updatePass', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(update)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.message == "actualizacion exitosa") {
+            window.location.href = '/michelin/home';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    });
+};
